d377a536b0b1e44e223a9e6e893d7bd177bea334
Haeb03 <bestosoadrian33@gmail.com>
2026-09-16 14:55:46 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ghea-tactico-02811-bosque30';
+const CACHE='ghea-tactico-02812-mundo30';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest',
 './assets/terreno-bosque.png','./assets/obstaculo-roca.png','./assets/obstaculo-arbol.png',
 './assets-miniaturas/guerrero-humano-abajo.png','./assets-miniaturas/guerrero-humano-arriba.png','./assets-miniaturas/guerrero-humano-izquierda.png','./assets-miniaturas/guerrero-humano-derecha.png',
