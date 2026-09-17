9f2dbef2a4e92725f04d155a436a954388b4c2e5
Haeb03 <bestosoadrian33@gmail.com>
2026-09-17 12:29:11 -0300

    Add files via upload

---
+++ assets-obstaculos/sw.js
@@ -0,0 +1,30 @@
+const CACHE='ghea-tactico-02828-obstacle-spacing-multicell-hotfix';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest',
+'./assets/afueras-arca-oeste.png',
+'./assets-obstaculos/arboles/arbol-01.png',
+'./assets-obstaculos/arboles/arbol-02.png',
+'./assets-obstaculos/arboles/arbol-03.png',
+'./assets-obstaculos/arboles/arbol-04.png',
+'./assets-obstaculos/arboles/arbol-05.png',
+'./assets-obstaculos/arbustos/arbusto-01.png',
+'./assets-obstaculos/arbustos/arbusto-03.png',
+'./assets-obstaculos/arbustos/arbusto-04.png',
+'./assets-obstaculos/arbustos/arbusto-05.png',
+'./assets-obstaculos/arbustos/arbusto-frambuesas.png',
+'./assets-obstaculos/madera/madera-01.png',
+'./assets-obstaculos/madera/madera-02.png',
+'./assets-obstaculos/madera/madera-03.png',
+'./assets-obstaculos/madera/madera-04.png',
+'./assets-obstaculos/madera/madera-05.png',
+'./assets-obstaculos/rocas/piedra-comun-01.png',
+'./assets-obstaculos/rocas/piedra-comun-02.png',
+'./assets-obstaculos/rocas/piedra-comun-03.png',
+'./assets-obstaculos/rocas/piedra-comun-04.png',
+'./assets-obstaculos/rocas/piedra-comun-05.png',
+'./assets-miniaturas/guerrero-humano-abajo.png','./assets-miniaturas/guerrero-humano-arriba.png','./assets-miniaturas/guerrero-humano-izquierda.png','./assets-miniaturas/guerrero-humano-derecha.png',
+'./assets-miniaturas/jabali-abajo.png','./assets-miniaturas/jabali-arriba.png','./assets-miniaturas/jabali-izquierda.png','./assets-miniaturas/jabali-derecha.png',
+'./assets-miniaturas/bandido-daga-abajo.png','./assets-miniaturas/bandido-daga-arriba.png','./assets-miniaturas/bandido-daga-izquierda.png','./assets-miniaturas/bandido-daga-derecha.png',
+'./assets-miniaturas/bandido-ballesta-abajo.png','./assets-miniaturas/bandido-ballesta-arriba.png','./assets-miniaturas/bandido-ballesta-izquierda.png','./assets-miniaturas/bandido-ballesta-derecha.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const x=r.clone();caches.open(CACHE).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request))));
